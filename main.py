f84f6685287a53554ea469d3e97b197eaa5c4cbb
Ashiquemk <ashiquemk25@gmail.com>
2024-04-13 10:57:59 +0530

    data ingestion done

---
+++ main.py
@@ -0,0 +1,13 @@
+from textsummarizer.pipeline.stage01_data_ingestion import DataIngestionPipeline
+from textsummarizer.logging import logger
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
